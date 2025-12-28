80055190eda5cef44dc3a6cc39a97e6f09747fb2
Sebastian Quintero Arias <bastian284c@gmail.com>
2025-12-27 23:35:02 -0500

    Quadratic solution script

--- Section 2/quadratic_equation_complex.py
+++ Section 2/quadratic_equation_complex.py
@@ -1,10 +1,14 @@
-'''Quadratic equation'''
+'''Quadratic equation with complex solutions'''
 
 import numpy as np
 
 a = float(input("Please enter the value of a: "))
 b = float(input("Please enter the value of b: "))
 c = float(input("Please enter the value of c: "))
+
+a = a + 0j
+b = b + 0j
+c = c + 0j
 
 x_1 = (-b + np.sqrt(b**2 - 4 * a * c))/(2*a)
 x_2 = (-b - np.sqrt(b**2 - 4 * a * c))/(2*a)
