21123f50a6defdee7d4b37389d96cd67375e3471
Sebastian Quintero Arias <bastian284c@gmail.com>
2025-12-28 16:32:52 -0500

    Introduction to Jupyter and Spider IDE

---
+++ Section 2/.ipynb_checkpoints/my_trip-checkpoint.py
@@ -0,0 +1,16 @@
+# Get inputs
+
+distance = 180.
+mpk = 3.9
+speed = 60.
+cost_per_kWh = 0.22
+
+# Calculate outputs
+time = distance / speed
+energy = distance / mpk
+cost = energy * cost_per_kWh
+
+# Print outputs
+print(time)
+print(energy)
+print(cost)
